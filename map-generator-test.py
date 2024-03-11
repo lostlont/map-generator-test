2e96d38074a3a96e1d3fe77dbbb9ff3ee5dea58f
Perjéssy, Lóránt <lostlont@users.noreply.github.com>
2024-03-11 18:17:03 +0100

    Added test usage of PyPNG

--- map-generator-test.py
+++ map-generator-test.py
@@ -1,7 +1,18 @@
 #!/bin/env python
+import png
 
 def run():
 	print("Test!")
+	pixels = []
+	for y in range(256):
+		row = []
+		for x in range(256):
+			row.append((x*y) % 256)
+			row.append((x+y) % 256)
+			row.append(abs(x-y) % 256)
+		pixels.append(row)
+	pngImage = png.from_array(pixels, 'RGB')
+	pngImage.save('out.png')
 
 if __name__ == '__main__':
 	run()
